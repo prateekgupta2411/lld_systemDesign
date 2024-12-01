dde11478235b165a5fe8f7fce1b7f5b1fd8df5a2
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2024-12-01 17:03:07 +0530

    Interface Segregation Principle (ISP)

---
+++ src/withOut_interfaceSegregationPrinciple/Defination.java
@@ -0,0 +1,21 @@
+package withOut_ISP;
+
+public class Defination {
+//    Interface Segregation Principle (ISP)
+//
+//    Yeh principle kehata hai ki:
+//
+//    Clients should not be forced to implement interfaces they do not use.
+//
+//    Iska matlab hai ki jab aap kisi class ko ek interface dete ho, to us interface mein sirf wo methods hone
+//    chahiye jo class ko zarurat ho. Agar ek class ko kuch methods ka use nahi hai, to usse un methods ko implement karne ka force nahi karna chahiye.
+
+//    The Interface Segregation Principle (ISP) ka basic idea hai:
+//
+//    Clients should not be forced to implement interfaces they do not use.
+//
+//    Yani, agar aapke paas ek bada interface hai jo multiple functionalities define karta hai, to clients (class jo is interface ko implement karte hain) ko wo functionalities implement karne ke liye force nahi karna chahiye jo unhe zarurat nahi hai.
+//    Simple Samajh:
+//
+//    Agar ek class ko ek interface dena hai, to wo interface sirf unhi methods ko include kare jo us class ko zarurat ho. Agar ek class ko kuch methods ka use nahi hai, to usse un methods ko implement karne ka force nahi karna chahiye.
+}
---
+++ src/with_interfaceSegregationPrinciple/Main.java
@@ -0,0 +1,4 @@
+package with_interfaceSegregationPrinciple;
+
+public class Main {
+}
